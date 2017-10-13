f3c63254f02c6a6fe86dff145640f3a5f50b2a3d
up201503447 <up201503447@fe.up.pt>
2017-10-13 12:31:46 +0100

    board prints correctly and we can get a piece

--- lear.pl
+++ lear.pl
@@ -4,37 +4,50 @@
 
 
 initialBoard([
-	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
-	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
-	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
-	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
-	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
-	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
-	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
-	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell]]).
-
-
+	[11, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+	[emptyCell, emptyCell, emptyCell, emptyCell, ol, emptyCell, emptyCell, emptyCell],
+	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+	[emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, 22]]).
 
 	
-lear:-initialBoard(Board), printInitialBoard(Board).
+lear:- initialBoard(Board), printRowSeparator, printBoard(Board), startGame(Board).
 
 
+startGame(Board):- getPiece(Board, 1, 2, X), write(X), nl.
 
-getPiece(Board, NLine, NColumn, Piece):- getElemPos(NLine, Board, Line), getElemPos(NColumn, Board, Piece).
 
-setPiece(PrevBoard, NLine, NColumn, Piece, Board):- setNLine(NLine, PrevBoard, NColumn, Piece, Board).
+%------------GET PIECE-------------
 
-%-----------------------------------
+getPiece(Board, NLine, NColumn, Piece):- 
+		getElemPos(NLine, Board, Line), 
+		getElemPos(NColumn, Line, Piece).
+
 
 getElemPos(1,[Element|_], Element).
-getElemPos(Pos,[_|Tail], Element):- Pos > 1, Next is Pos-1, getElemPos(Next, Tail, Element).
+getElemPos(Pos,[_|Tail], Element):-
+		Pos > 1, 
+		Next is Pos-1, 
+		getElemPos(Next, Tail, Element).
 
-%-----------------------------------
+%------------SET PIECE-------------
 
-setNLine(1, [Line|Tail], NColumn, Piece, [NewLine| Tail]):- setNColumn(NColumn, Line, Piece, NewLine).
-setNLine(Pos, [Line|Tail], NColumn, Piece, [Line| NewTail]):- Pos > 1, Next is Pos-1, setNLine(Next, Line, NColumn, Piece, NewTail).
+
+setPiece(PrevBoard, NLine, NColumn, Piece, Board):- 
+		setNLine(NLine, PrevBoard, NColumn, Piece, Board).
+
+
+setNLine(1, [Line|Tail], NColumn, Piece, [NewLine| Tail]):- 
+		setNColumn(NColumn, Line, Piece, NewLine).
+setNLine(Pos, [Line|Tail], NColumn, Piece, [Line| NewTail]):- 
+		Pos > 1, Next is Pos-1, 
+		setNLine(Next, Line, NColumn, Piece, NewTail).
 
 setNColumn(1, [_|Tail], Piece, [Piece| Tail]).
-setNColumn(Pos, [X|Tail], Pos, [X|NewTail]):- Pos > 1, Next is Pos-1, setNColumn(Next, Tail, Piece, NewTail).
-
-%-----------------------------------
+setNColumn(Pos, [X|Tail], Pos, [X|NewTail]):- 
+		Pos > 1, 
+		Next is Pos-1, 
+		setNColumn(Next, Tail, Piece, NewTail).
